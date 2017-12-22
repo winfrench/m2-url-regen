63da5c055847b74f07c8c45777c446636ff3bef6
William French <will@jamersan.com>
2017-12-22 15:20:19 -0600

    Add Module's registration.php

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Regenerate URL Rewrites via cli
+ *
+ * @package WinFrench_RegenUrl
+ * @author William French <winfrench@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'WinFrench_RegenUrl',
+    __DIR__
+);
